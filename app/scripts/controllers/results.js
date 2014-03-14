00a936794047a5788ce1770a4c78651f47d80fa1
Andrew Keym <andrew.keym@gmail.com>
2014-03-13 22:34:51 -0400

    cleaned up styles

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -2,13 +2,27 @@
 
 angular.module('lastfmApp')
   .controller('ResultsCtrl', function ($scope, Artists, $routeParams) {
-    //$scope.artists = Artists.query({ 'artist':$routeParams.artist });
+    // search echo
     $scope.search = $routeParams.artist;
 
     Artists.getArtists($routeParams.artist)
       .then(function (data) {
+        // vars for grid layout
+        var maxRows = 30;
+        var maxCols = 4;
+        var counter = 0;
+
         $scope.artists = data;
         $scope.total = data.results['opensearch:totalResults'];
+        $scope.rows = [];
+
+        for( var i=0 ; i < maxRows; i++){
+          $scope.rows.push([]);
+          for( var j=0 ; j < maxCols; j++){
+            $scope.rows[i][j] = $scope.artists.results.artistmatches.artist[counter];
+            counter++;
+          }
+        }
       }, function(error) {
         // promise rejected
         console.log(error);

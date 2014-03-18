806de67b2f5c0781dcab0850aac27f6f71396327
Andrew Keym <andrew.keym@gmail.com>
2014-03-17 22:27:44 -0400

    Adding some tests for the results page

--- test/spec/controllers/results.js
+++ test/spec/controllers/results.js
@@ -6,17 +6,36 @@ describe('Controller: ResultsCtrl', function () {
   beforeEach(module('lastfmApp'));
 
   var ResultsCtrl,
+    $httpBackend,
+    location,
+    routeParams,
     scope;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($controller, $rootScope) {
+  beforeEach(inject(function ($controller, $rootScope, $injector,  $location) {
+    // Set up the mock http service responses
+    $httpBackend = $injector.get('$httpBackend');
+
+    // Get hold of a scope (i.e. the root scope)
+    //$rootScope = $injector.get('$rootScope');
+
+    location = $location.path('/search/tool');
     scope = $rootScope.$new();
+    routeParams = {};
+    routeParams.artist = 'tool';
+    // The $controller service is used to create instances of controllers
+    //$controller = $injector.get('$controller');
     ResultsCtrl = $controller('ResultsCtrl', {
-      $scope: scope
+      $scope: scope,
+      $routeParams : routeParams
     });
   }));
 
-  it('sample test', function () {
-    expect(true);
+  it('should echo the searched artist', function () {
+    expect(scope.search).toBe('tool');
+  });
+
+  it('should show "Searching..." as the default context message', function () {
+    expect(scope.resultMessage).toBe('Searching...');
   });
 });

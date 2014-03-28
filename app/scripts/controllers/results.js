228deb0c9b225f74b328e95c4c50a5d178abe8d5
Andrew Keym <andrew.keym@gmail.com>
2014-03-27 20:17:16 -0400

    Added infinite scrolling feature

--- app/scripts/controllers/results.js
+++ app/scripts/controllers/results.js
@@ -10,47 +10,56 @@ angular.module('lastfmApp')
     $scope.total = 0;
     $scope.totalShown = 0;
     $scope.showMore = false;
+    $scope.populating = false;
 
     $scope.populate = function (page) {
-      Artists.getArtists($routeParams.artist, page)
-      .then(function (data) {
-        var artistResults = data.results.artistmatches.artist;
-        $scope.artists = angular.isArray(artistResults) ? artistResults : [artistResults];
-        $scope.total = Number(data.results['opensearch:totalResults']);
-        $scope.itemsPerPage = Number(data.results['opensearch:itemsPerPage']);
-        $scope.resultMessage = $scope.total + ' matches were found';
-        $scope.totalShown = $scope.page * $scope.itemsPerPage;
+      console.log(page);
+      // check if we're already populating
+      if (!$scope.populating) {
+        $scope.populating = true;
+        Artists.getArtists($routeParams.artist, page)
+        .then(function (data) {
+          var artistResults = data.results.artistmatches.artist;
+          $scope.artists = angular.isArray(artistResults) ? artistResults : [artistResults];
+          $scope.total = Number(data.results['opensearch:totalResults']);
+          $scope.itemsPerPage = Number(data.results['opensearch:itemsPerPage']);
+          $scope.resultMessage = $scope.total + ' matches were found';
+          $scope.totalShown = $scope.page * $scope.itemsPerPage;
+          $scope.populating = false;
 
-        // vars for grid layout
-        $scope.rows = $scope.rows || [];
-        var maxCols = 4;
-        var maxRows = Math.round($scope.itemsPerPage / maxCols) * page;
-        var counter = 0;
+          // vars for grid layout
+          $scope.rows = $scope.rows || [];
+          var maxCols = 4;
+          var maxRows = Math.round($scope.itemsPerPage / maxCols) * page;
+          var counter = 0;
 
-        // restructure data for grid layout
-        for( var i=$scope.rows.length; i < maxRows; i++){
-          if ($scope.artists[counter]) {
-            $scope.rows.push([]);
-            for( var j=0 ; j < maxCols; j++){
-              if ($scope.artists[counter]) {
-                $scope.rows[i][j] = $scope.artists[counter];
-                counter++;
+          // restructure data for grid layout
+          for( var i=$scope.rows.length; i < maxRows; i++){
+            if ($scope.artists[counter]) {
+              $scope.rows.push([]);
+              for( var j=0 ; j < maxCols; j++){
+                if ($scope.artists[counter]) {
+                  $scope.rows[i][j] = $scope.artists[counter];
+                  counter++;
+                }
               }
+            } else {
+              counter++;
             }
-          } else {
-            counter++;
           }
-        }
-        $scope.isShownMoreThanTotal();
-      }, function(error) {
-        // promise rejected
-        console.log(error);
-      });
+          $scope.isShownMoreThanTotal();
+        }, function(error) {
+          // promise rejected
+          console.log(error);
+        });
+      }
     };
 
     $scope.nextPage = function () {
-      $scope.page = Number($scope.page) + 1;
-      $scope.populate($scope.page);
+      if (!$scope.populating && $scope.showMore) {
+        $scope.page = Number($scope.page) + 1;
+        $scope.populate($scope.page);
+      }
     };
 
     $scope.isShownMoreThanTotal = function () {
--- app/scripts/directives/infinitescroll.js
+++ app/scripts/directives/infinitescroll.js
@@ -1,12 +1,21 @@
 'use strict';
-/*
+
 angular.module('lastfmApp')
-  .directive('infiniteScroll', function () {
+  .directive('infiniteScroll', function ($window) {
     return {
-      template: '<div></div>',
-      restrict: 'E',
-      link: function postLink(scope, element, attrs) {
-        element.text('this is the infiniteScroll directive');
+      link: function (scope, element, attrs) {
+        var elementBottom, windowBottom;
+        $window = angular.element($window);
+        console.log(scope);
+        $window.bind('scroll', function() {
+          windowBottom = $window.height() + $window.scrollTop();
+          elementBottom = element.offset().top + element.innerHeight();
+          if (windowBottom >= elementBottom) {
+            scope.$apply(attrs.infiniteScroll);
+          }
+        });
+
       }
     };
-  });*/
+  });
+

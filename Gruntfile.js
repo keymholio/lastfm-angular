b4e70a4f2511593c3b1dc62cbd105e182bb7d135
Andrew Keym <andrew.keym@gmail.com>
2014-04-11 11:45:53 -0400

    Grunt tweak

--- Gruntfile.js
+++ Gruntfile.js
@@ -387,7 +387,7 @@ module.exports = function (grunt) {
     'useminPrepare',
     'concurrent:dist',
     'autoprefixer',
-    'concat',
+    //'concat',
     'ngmin',
     'copy:dist',
     'cdnify',

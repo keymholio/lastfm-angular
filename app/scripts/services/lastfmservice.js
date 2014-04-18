08a1f7e8d2e2eece4f3097c883d17c7968b27ae1
Andrew Keym <andrew.keym@gmail.com>
2014-04-17 20:49:47 -0500

    Made search service a 'service' instead of a 'factory'

--- app/scripts/services/lastfmservice.js
+++ app/scripts/services/lastfmservice.js
@@ -1,34 +1,32 @@
 'use strict';
 
-angular.module('lastfmServices', ['ngResource'])
-  .factory('Artists', function ($http, $q) {
-      var one = 1;
-      one = 1;
-      return {
-          getArtists: function(artist, page) {
-              // the $http API is based on the deferred/promise APIs exposed by the $q service
-              // so it returns a promise for us by default
-              return $http.get('http://ws.audioscrobbler.com/2.0/', {
-                  params: {
-                    'method': 'artist.search',
-                    'artist': artist,
-                    'api_key': 'aceab0ee7a1b569b1cec10bac112a84e',
-                    'page': page,
-                    'limit': 24,
-                    'format': 'json'
-                  }
-                }).then(function(response) {
-                    if (typeof response.data === 'object') {
-                      return response.data;
-                    } else {
-                      // invalid response
-                      return $q.reject(response.data);
-                    }
-                  }, function(response) {
-                    // something went wrong
-                    return $q.reject(response.data);
-                  }
-              );
-            }
-        };
-    });
+angular.module("lastfmServices").service("Artists",
+function($http, $q) {
+  return {
+    getArtists: function(artist, page) {
+      // the $http API is based on the deferred/promise APIs exposed by the $q service
+      // so it returns a promise for us by default
+      return $http.get('http://ws.audioscrobbler.com/2.0/', {
+        params: {
+          'method': 'artist.search',
+          'artist': artist,
+          'api_key': 'aceab0ee7a1b569b1cec10bac112a84e',
+          'page': page,
+          'limit': 24,
+          'format': 'json'
+        }
+      }).then(function(response) {
+          if (typeof response.data === 'object') {
+            return response.data;
+          } else {
+            // invalid response
+            return $q.reject(response.data);
+          }
+        }, function(response) {
+          // something went wrong
+          return $q.reject(response.data);
+        }
+      );
+    }
+  };
+});
